4951da04e943f2ac5952938741a7c76caf788d15
Amardeep <as4214988@gmail.com>
2020-01-28 18:38:22 -0500

    Commit 1 to IOS LAB 3

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Amardeep Singh . on 2020-01-28.
+//  Copyright © 2020 Humber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
